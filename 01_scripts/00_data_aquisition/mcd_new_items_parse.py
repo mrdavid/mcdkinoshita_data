23982be4b89684a91cf66fcfd26e76b34b4f990b
David <drwebservice@gmail.com>
2017-08-19 08:50:14 +0900

    Parse new item data

--- 01_scripts/00_data_aquisition/mcd_new_items_parse.py
+++ 01_scripts/00_data_aquisition/mcd_new_items_parse.py
@@ -1,25 +1,42 @@
+# Preable
 import argparse
 import os.path
 import sys
 from bs4 import BeautifulSoup
+import re
+import csv
 
+# Which file should we parse?
 parser = argparse.ArgumentParser(description='Process file name')
 parser.add_argument('file', help='Filename')
 args = parser.parse_args()
 
 print("Parsing downloaded file from python.")
 
+# Check that input file has been downloaded
 if not os.path.isfile(args.file):
     print("Source file does not exist. Exiting.")
     sys.exit(1)
 
 file = open(args.file, "r")
-
+#os.getcwd()
+#file = open("00_data/mcd_homepage_new_items_raw.html", "r")
 parsed_html = BeautifulSoup(file.read(), "lxml")
 
-#print(parsed_html)
-print(parsed_html.find_all("div", _class="item_wrap"))
+output = [['id','item_name', 'item_date']]
 
-for lists in parsed_html.find_all("ul", _class="list_item"):
+idd = 0
+for lists in parsed_html.find_all("ul", class_="list_item"):
     for li in lists.find_all("li"):
-        print(li.h3)
+        item_name = li.h3.text # name of the product        
+        mat = li.p.text
+        res = re.search('^(\d{4}\/\d{1,2}\/\d{1,2})', mat)
+        item_date = res.group(0) # date when it was first sold
+        
+        output.append([idd, item_name, item_date])
+        idd += 1 # keep a unique ID just in case
+        
+# output to css
+with open(args.file+'.csv', 'w') as myfile:
+    wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    wr.writerows(output)
